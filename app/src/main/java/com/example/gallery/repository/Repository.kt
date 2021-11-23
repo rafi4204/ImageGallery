0a7ee7afad02eb6a648ec546068424d9379302d0
rafi <muhammad.farook@brainstation-23.com>
2021-11-23 20:13:52 +0600

    refactor: remove unused code

--- app/src/main/java/com/example/gallery/repository/Repository.kt
+++ app/src/main/java/com/example/gallery/repository/Repository.kt
@@ -6,36 +6,21 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.gallery.data.ImageModel
 import com.example.gallery.network.ApiService
-import com.example.gallery.offlineDb.DoggoImagePagingSource
-import com.example.gallery.utils.AppHelper.DEFAULT_PAGE_SIZE
 import com.example.gallery.offlineDb.db.AppDatabase
 import com.example.gallery.offlineDb.mediator.ImageMediator
-
+import com.example.gallery.utils.AppHelper.DEFAULT_PAGE_SIZE
 import dagger.hilt.android.scopes.ActivityRetainedScoped
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
+
 @ActivityRetainedScoped
 @ExperimentalPagingApi
-class Repository @Inject constructor(private val apiService: ApiService, private val appDatabase: AppDatabase) {
-
-    fun getImages(i: Int): Flow<List<ImageModel>> {
-        return flow {
-            val imageList = apiService.getImages(i,100)
-
-            emit(imageList)
-        }.flowOn(Dispatchers.IO)
-    }
+class Repository @Inject constructor(
+    private val apiService: ApiService,
+    private val appDatabase: AppDatabase
+) {
 
 
-    fun getImages(pagingConfig: PagingConfig = getDefaultPageConfig()): Flow<PagingData<ImageModel>> {
-        return Pager(
-            config = pagingConfig,
-            pagingSourceFactory = { DoggoImagePagingSource(apiService) }
-        ).flow
-    }
     private fun getDefaultPageConfig(): PagingConfig {
         return PagingConfig(pageSize = DEFAULT_PAGE_SIZE, enablePlaceholders = true)
     }
