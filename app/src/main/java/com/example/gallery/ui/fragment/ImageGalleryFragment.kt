50a4111bdd3bc20a07a5d35825a2dfd3739c07cf
rafi <muhammad.farook@brainstation-23.com>
2021-11-23 11:07:45 +0600

    show image list using image adapter in ImageGalleryFragment.kt

--- app/src/main/java/com/example/gallery/ui/fragment/ImageGalleryFragment.kt
+++ app/src/main/java/com/example/gallery/ui/fragment/ImageGalleryFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.paging.ExperimentalPagingApi
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.gallery.R
+import com.example.gallery.data.ImageModel
 import com.example.gallery.databinding.FragmentImageGalleryBinding
+import com.example.gallery.ui.adapter.ImageAdapter
 import com.example.gallery.ui.viewModel.ImageGalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 @ExperimentalPagingApi
@@ -37,7 +43,28 @@ class ImageGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getNavController()
+        val adapter = ImageAdapter(requireContext())
+        binding.rvNewsList.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.rvNewsList.adapter = adapter
+        lifecycleScope.launchWhenCreated {
+            viewModel.getImage()
+        }
+        viewModel.list.observe(viewLifecycleOwner, {
+            lifecycleScope.launch {
+                adapter.submitData(it)
+            }
+        })
+        adapter.listener = object : ImageAdapter.ClickListener{
+            override fun onClick(model: ImageModel) {
+                val bundle = bundleOf("url" to model.download_url)
+                navController?.navigate(
+                    R.id.action_imageGalleryFragment_to_fullScreenImageFragment,
+                    bundle
+                )
+            }
 
+        }
         toolbar = requireActivity().findViewById(R.id.toolbar)
         toolbar?.title = "Image Gallery"
 
@@ -49,6 +76,5 @@ class ImageGalleryFragment : Fragment() {
             requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment?.navController
     }
-
 
 }
